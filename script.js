226ebba658dd9a558b5375641bed7a9870799edd
calista <clsta.lee@gmail.com>
2020-05-13 09:32:28 +0700

    Add additional buttons

--- script.js
+++ script.js
@@ -1,37 +1,85 @@
 const container = document.querySelector('.container');
-const buttonGenerate = document.querySelector('button');
-const divs = [];
+// const buttonGenerate = document.querySelector('#generateButton');
+// const toggleGridButton = document.querySelector('#toggleGridButton');
+// const rainbowButton = document.querySelector('#rainbowButton');
+// const pencilButton = document.querySelector('#pencilButton');
 
-function generateGrid(num){
-	let size = (500/num).toFixed(4) + "px"; 
-	for(let i = 0; i < (num*num) ; i++){
-		
-			const div = document.createElement('div');
-			div.setAttribute('id','grid')
-			div.style.cssFloat = "left";
-			div.style.backgroundColor = "lightgrey";
-			div.style.width = size;
-			div.style.height = size;
-			div.addEventListener('mouseenter', () => div.style.backgroundColor = "grey");
-			container.appendChild(div);
-		
-	}
-}
-
-function test(num){
+function generateCanvas(num){
 	container.innerHTML = "";
 	container.style.gridTemplateColumns = "repeat(" + num + ", 1fr)";
 	container.style.gridTemplateRows = "repeat(" + num + ", 1fr)";
 	for(let i = 0; i < Math.pow(num,2) ; i++){
 		const div = document.createElement('div');
+		div.setAttribute('id', 'grid');
 		div.classList.add("test");
-		div.classList.add("grid-line");
-		divs.push(div);
+		div.addEventListener('mouseover', function(e){
+			div.style.backgroundColor = "black";
+		})
 		container.appendChild(div);		
 	}
 }
 
-buttonGenerate.addEventListener('click', function(e){
+function randomColor(){
+	let color =  Math.floor(Math.random() * Math.floor(255));
+	return color;
+}
+
+generateButton.addEventListener('click', function(e){
 	let size = prompt('Enter canvas size');
-	test(size);
+	generateCanvas(size);
 });
+
+toggleGridButton.addEventListener('click', function(e){
+	const divs = document.querySelectorAll('#grid');
+	divs.forEach(div => div.classList.toggle('grid-line'));
+})
+
+rainbowButton.addEventListener('click', function(e){
+	const divs = document.querySelectorAll('#grid');
+	divs.forEach(div => div.addEventListener('mouseover', function(e){
+		div.style.backgroundColor = 'rgb(' + randomColor() + ',' + randomColor() + ',' + randomColor() + ')';	
+	}))
+})
+
+pencilButton.addEventListener('click', function(e){
+	const divs = document.querySelectorAll('#grid');
+	divs.forEach(div => div.addEventListener('mouseover', function(e){
+		div.style.backgroundColor = "black";	
+	}))	
+})
+
+eraserButton.addEventListener('click', function(e){
+	const divs = document.querySelectorAll('#grid');
+	divs.forEach(div => div.addEventListener('mouseover', function(e){
+		div.style.backgroundColor = "";	
+	}))	
+})
+
+shaderButton.addEventListener('click', function(e){
+	const divs = document.querySelectorAll('#grid');
+	divs.forEach(div => div.addEventListener('mouseover', function(e){
+		div.style.setProperty("--darken","-10%");	
+	}))		
+})
+
+preciseButton.addEventListener('click', function(e){
+	const divs = document.querySelectorAll('#grid');
+	let mouseDown = 0;
+	divs.forEach(div => {
+		div.onmouseDown = function(){
+			mouseDown = 1;
+		}
+		div.onmouseUp = function(){
+			mouseDown = 0;
+		}
+
+		if(mouseDown == 1){
+			div.style.backgroundColor = 'black';
+		}
+	})	
+})
+
+clearButton.addEventListener('click', function(e){
+	const divs = document.querySelectorAll('#grid');
+	divs.forEach(div => div.style.backgroundColor = "");
+})
